64195c9c19fa92bfbf85c0f3376d529508dfc34c
liufeng <fengliu018@gmail.com>
2021-04-25 20:04:18 +0800

    feat(config): new config

---
+++ config.lua
@@ -0,0 +1,27 @@
+windowHotkey = {'control','command'}
+launcherHotkey = {'option'}
+inputHotkey = {'option'}
+
+applist = {
+    {shortcut = 'Q',appname = 'QQ'},
+    {shortcut = 'G',appname = 'Google Chrome'},
+    {shortcut = 'C',appname = 'Visual Studio Code'},
+    {shortcut = 'I',appname = 'iTerm'},
+    {shortcut = 'W',appname = 'WeChat'},
+    {shortcut = 'P',appname = 'PicGo'},
+    {shortcut = 'E',appname = '企业微信'},
+    {shortcut = 'F',appname = '丰声'},
+    {shortcut = 'O',appname = 'Microsoft Outlook'},
+    {shortcut = 'M',appname = '网易云.app'},
+}
+
+appInputMethod = {
+    {'/Applications/iTerm.app', 'English'},
+    -- {'/Applications/Visual Studio Code.app', 'English'},
+    {'/Applications/Google Chrome.app', 'Chinese'},
+    {'/System/Library/CoreServices/Finder.app', 'English'},
+    {'/Applications/WeChat.app', 'Chinese'},
+    {'/Applications/System Preferences.app', 'English'},
+    {'/Applications/QQ.app', 'Chinese'},
+    {'/Applications/企业微信.app', 'Chinese'},
+}
---
+++ init.lua
@@ -0,0 +1,13 @@
+require "modules.reload"
+require "config"
+require "modules.window"
+require "modules.launcher"
+require "modules.defaultInput"
+-- require "modules.bluetoothSleep"
+
+hs.hotkey.bind({'cmd', 'shift'}, 'h', function()
+	hs.alert('Hello World') 
+	speaker = hs.speech.new()
+	speaker:speak("Hammerspoon is online")
+	hs.notify.new({title="Hammerspoon launch", informativeText="Boss, at your service"}):send()
+end)
